4245811d51af25bda0b08ea2b1ef2ce67765ff79
Victor Leme <victorlemefernandes@gmail.com>
2024-03-27 21:56:35 -0300

    Ajustes e talvez finalizado

--- segundoTrabalho/trabalho_LOAN/utilizandoPythonPuro.py
+++ segundoTrabalho/trabalho_LOAN/utilizandoPythonPuro.py
@@ -3,6 +3,7 @@ from sklearn.metrics import confusion_matrix, accuracy_score, f1_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy
 
 # Salvando o caminho dos dados
 url = "segundoTrabalho\\trabalho_LOAN\\data-set\\arquivoKNN_normalizado.csv"
@@ -13,32 +14,50 @@ names = ['Loan ID', 'Gender', 'Married', 'Dependents', 'Education', 'Self_Employ
 # Lê o dataset
 df = pd.read_csv(url, names=names)
 
-X = df.drop('Loan_Status', axis=1) # Remove a coluna 'Loan_Status' do dataframe
-y = df['Loan_Status'] # Armazena os dados da coluna 'Loan_Status' na variável y
+todasAcuracias = []
+todasMedidasF = []
+todasMatrizesConfusao = []
 
-# Separa dados de testes e treino
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+def knn(k, colunasDrops):
+    X = df.drop(colunasDrops, axis=1) # Remove a coluna 'Loan_Status' do dataframe
+    y = df['Loan_Status'] # Armazena os dados da coluna 'Loan_Status' na variável y
 
-# Faz a padronização dos dados de treinamento e teste
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+    # Separa dados de testes e treino e coloca o tamanho do teste em 30%
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-# Cria instância do classificador KNN com n_neighbors vizinhos e depois o treina com os dados de treinamento padronizados
-knn = KNeighborsClassifier(n_neighbors=11)
-knn.fit(X_train, y_train)
+    # Faz a padronização dos dados de treinamento e teste
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
 
-# Previsões nos dados de teste
-y_pred = knn.predict(X_test)
+    # Cria instância do classificador KNN com n_neighbors vizinhos e depois o treina com os dados de treinamento padronizados
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
 
-# Calculo da acurácia
-accuracy = accuracy_score(y_test, y_pred)
-print("Acurácia:", accuracy)
+    # Previsões nos dados de teste
+    y_pred = knn.predict(X_test)
 
-# Medida F
-f1 = f1_score(y_test, y_pred, average='weighted')
-print(f'Medida F: {f1}')
+    # Calculo da acurácia
+    accuracy = accuracy_score(y_test, y_pred)
+    todasAcuracias.append(accuracy)
 
-# Calculando matriz de confusão
-conf_matrix = confusion_matrix(y_test, y_pred)
-print(f'Matriz de confusão: \n{conf_matrix}')
+    # Medida F
+    f1 = f1_score(y_test, y_pred, average='weighted')
+    todasMedidasF.append(f1)
+
+    # Calculando matriz de confusão
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    todasMatrizesConfusao.append(conf_matrix)
+
+
+# Colunas/atributos que podem ser retirados da classificação
+colunasDropadas = ['Loan_Status']
+
+for i in range(0, 1000):
+    knn(11, colunasDropadas) # knn(k = vizinhos, colunasDropadas)
+
+print(f'Média da acurácia: {numpy.mean(todasAcuracias)}')
+print(f'Média da medida F: {numpy.mean(todasMedidasF)}')
+
+matrizAuxiliar = numpy.array(todasMatrizesConfusao) # Transforma a matriz em tridimensional p/ ser tratada no numpy
+print(f'Média das matrizes de confusão: \n{numpy.mean(matrizAuxiliar, axis=0)}')
